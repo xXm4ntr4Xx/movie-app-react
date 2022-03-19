a53ae44949719950d48c9f65d7d16953e840769f
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-03-19 12:41:32 +0000

    up

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("Naruto");
   }, []);
 
   const searchMovies = async (title) => {
